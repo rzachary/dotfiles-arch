eeb68ee47ffe5eaa8bba9085bd3b293fe8d87cfe
Rickey Zachary <rzachary@rickeyzachary.com>
2021-02-14 12:21:05 -0500

    added bin and multimedia files

---
+++ installers/install-multimedia.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install photo packages
+sudo pacman -S gimp inkscape libgphoto2 shotwell
+
+# Install Sound Packages
+sudo pacman -S alsa-firmware alsa-plugins alsa-tools alsa-utils
+
+# Install Multimedia Packages
+sudo pacman -S deadbeef moc vlc pianobar
+
+# Install Video Editor i don not need this at the moment
+# sudo pacman -S kdenlive
+
---
+++ installers/install-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install additional Teminals
+sudo pacman -S alacritty
+
+# Install Multiplexer
+sudo pacman -S tmux
+
+# Install Powerline
+sudo pacman -S powerline powerline-fonts powerline-vim
