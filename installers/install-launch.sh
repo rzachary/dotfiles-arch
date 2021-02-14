5beecb2473b96848f206712d2e4eaae84ee6e4b8
Rickey Zachary <rzachary@rickeyzachary.com>
2021-02-13 19:46:17 -0500

    added installer for startup apps

---
+++ installers/install-launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# lxsession install
+sudo pacman -S lxsession
+
+# nm-applet install
+sudo pacman -S network-manager-applet
+
+# trayer install
+sudo pacman -S trayer
+
+# dolphin install
+sudo pacman -S dolphin
