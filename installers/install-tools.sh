584d41ecab6f546a34e8cb534f642b05b84a2a5a
Rickey Zachary <rzachary@rickeyzachary.com>
2021-02-15 22:19:20 -0500

    updated installers with new shells added initial config files

--- installers/install-tools.sh
+++ installers/install-tools.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Install Shells
+sudo pacman -S zsh zsh-completions
+
+# Install main terminal 
+sudo pacman -S xterm termite
+
 # Install additional Teminals
 sudo pacman -S alacritty
+sudo pacman -S rxvt-unicode
 
 # Install Exa
 sudo pacman -S exa
